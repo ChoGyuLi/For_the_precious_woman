f6b1f156806e20cd1f0aa89f52ba7d86b8725046
mac <chogyuli53@gmail.com>
2023-08-04 11:41:40 +0900

    finish_project

---
+++ app/src/main/java/kr/ac/baekseok/for_the_precious_woman/Notice.java
@@ -0,0 +1,16 @@
+package kr.ac.baekseok.for_the_precious_woman;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+public class Notice extends User{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.notice);
+
+    }
+}
